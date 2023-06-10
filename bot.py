fbd24e690b4e7e6c00bbe4233ff6c19d4e465873
Nathan Higley <contact@nathanhigley.com>
2023-06-10 10:01:55 -0500

    feat: enable data and headers

--- bot.py
+++ bot.py
@@ -16,12 +16,14 @@ class Command:
     desc: str
     resp: str
     url: str
+    headers: str
+    data: str
 
-def cmd_builder(name, desc, resp, url):
+def cmd_builder(name, desc, resp, url, headers, data):
 
     return"@bot.command(description='" + desc + """')
 async def """ + name + """(ctx):
-    req = requests.post('""" + url + """')
+    req = requests.post('""" + url + """', headers=""" + headers + """, data=""" + data + """)
     await ctx.respond(f\"""" + resp + "\")"
 
 def get_commands():
@@ -29,12 +31,19 @@ def get_commands():
     dishook_prefix = "DISHOOK_COMMAND_"
     i = 0
     while os.getenv(str(dishook_prefix + str(i))):
+        current_prefix = str(dishook_prefix + str(i))
         cmd = Command(
-            name=str(os.getenv(str(dishook_prefix + str(i)))),
-            desc=str(os.getenv(str(dishook_prefix + str(i) + "_DESCRIPTION"))),
-            resp=str(os.getenv(str(dishook_prefix + str(i) + "_RESPONSE"))),
-            url=str(os.getenv(str(dishook_prefix + str(i) + "_URL")))
+            name=str(os.getenv(current_prefix)),
+            desc=str(os.getenv(str(current_prefix + "_DESCRIPTION"))),
+            resp=str(os.getenv(str(current_prefix + "_RESPONSE"))),
+            url=str(os.getenv(str(current_prefix  + "_URL"))),
+            headers="{}",
+            data="{}"
         )
+        if os.getenv(str(dishook_prefix + "_HEADERS")):
+            cmd.headers = str(os.getenv(str(dishook_prefix + "_HEADERS")))
+        if os.getenv(str(dishook_prefix + "_DATA")):
+            cmd.headers = str(os.getenv(str(dishook_prefix + "_DATA")))
         command_list.append(cmd) 
         i += 1
     return command_list
@@ -47,7 +56,7 @@ if __name__ == '__main__':
 
     for cmd in commands:
         print("Loading command: '" + cmd.name + "' with desc: '" + cmd.desc + "'")
-        exec(cmd_builder(cmd.name, cmd.desc, cmd.resp, cmd.url))
+        exec(cmd_builder(cmd.name, cmd.desc, cmd.resp, cmd.url, cmd.headers, cmd.data))
     
     print("Success.  Starting dishook...")
     bot.run(token)
